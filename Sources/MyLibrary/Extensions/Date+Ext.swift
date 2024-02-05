c05143a1e80400e5b069b56816d3f75a88efc9c1
dai <phamdaiit@gmail.com>
2024-02-05 15:56:52 +0700

    Update Date+Ext.swift

--- Sources/MyLibrary/Extensions/Date+Ext.swift
+++ Sources/MyLibrary/Extensions/Date+Ext.swift
@@ -7,6 +7,27 @@
 
 import Foundation
 
+public struct DefaultRangeMonth: RangeMonthProtocol {
+    public var distanceOfMinMonthToCurrent: Int {
+        get {
+            return -12
+        } set {}
+    }
+    
+    public var distanceOfMaxMonthToCurrent: Int {
+        get {
+            var calendar = Calendar(identifier: .gregorian)
+            calendar.firstWeekday = 2
+            let currenDate = calendar.component(.day, from: Date())
+            if currenDate >= 20 {
+                return 1
+            } else {
+                return 0
+            }
+        } set {}
+    }
+}
+
 public protocol RangeMonthProtocol {
     
     /// example 11 month ago then should be: -11, should be an uninteger
