ed3b58e15742ea81b974cd778fb98144ee85f830
dai <phamdaiit@gmail.com>
2024-02-07 22:44:28 +0700

    Update String+Ext.swift

--- Sources/MyLibrary/Extensions/String+Ext.swift
+++ Sources/MyLibrary/Extensions/String+Ext.swift
@@ -244,6 +244,7 @@ public extension String {
             let df = DateFormatter()
             df.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
             let locale = Locale(identifier: "en_US_POSIX")
+            df.timeZone = TimeZone(identifier: "UTC")
             df.locale = locale
             let dateFromString = df.date(from: self)
             return dateFromString
