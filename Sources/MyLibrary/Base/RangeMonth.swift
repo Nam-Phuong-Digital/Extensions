6b39dc4facbb88835e587d11ef7e095d1cbb02bb
dai <phamdaiit@gmail.com>
2024-02-08 17:34:18 +0700

    Update RangeMonth.swift

--- Sources/MyLibrary/Base/RangeMonth.swift
+++ Sources/MyLibrary/Base/RangeMonth.swift
@@ -12,34 +12,26 @@ open class RangeMonth {
     public init() {}
     
     /// example 11 month ago then should be: -11, should be an uninteger
-    var distanceOfMinMonthToCurrent:Int = -12
+    public var distanceOfMinMonthToCurrent:Int = -12
     
     /// example 1 month next then should be: 1 shoud ble an integer
-    var distanceOfMaxMonthToCurrent:Int = 1
+    public var distanceOfMaxMonthToCurrent:Int = 1
 }
 
 public class DefaultRangeMonth: RangeMonth {
     
     public override init() {
         super.init()
-    }
-    
-    public override var distanceOfMinMonthToCurrent: Int {
-        get {
-            return -12
-        } set {}
-    }
-    
-    public override var distanceOfMaxMonthToCurrent: Int {
-        get {
-            var calendar = Calendar(identifier: .gregorian)
-            calendar.firstWeekday = 2
-            let currenDate = calendar.component(.day, from: Date())
-            if currenDate >= 20 {
-                return 1
-            } else {
-                return 0
-            }
-        } set {}
+        var max = 0
+        var calendar = Calendar(identifier: .gregorian)
+        calendar.firstWeekday = 2
+        let currenDate = calendar.component(.day, from: Date())
+        if currenDate >= 20 {
+            max = 1
+        } else {
+            max = 0
+        }
+        distanceOfMaxMonthToCurrent = max
+        distanceOfMinMonthToCurrent = -12
     }
 }
