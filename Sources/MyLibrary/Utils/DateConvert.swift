7096a79616d82346935f1aeba6cc33baa696f950
dai <phamdaiit@gmail.com>
2024-02-08 11:33:03 +0700

    Update DateConvert.swift

--- Sources/MyLibrary/Utils/DateConvert.swift
+++ Sources/MyLibrary/Utils/DateConvert.swift
@@ -9,43 +9,63 @@ import Foundation
 
 public class DateConvert {
     
-    public var date:Date?
-    public var dateString:String?
+    private var date:Date?
+    private var dateString:String?
     
-    public func fromDateUTCoString(
+    public init(
+        date: Date? = nil,
+        dateString: String? = nil,
+        formatOutput:String = "yyyy-MM-dd'T'HH:mm:ss"
+    ) {
+        if let dateString {
+            self.dateString = dateString
+            fromStringToDate(dateString: dateString)
+        }
+        if let date {
+            self.date = date
+            fromDateToString(date: date, format: formatOutput)
+        }
+    }
+    
+    public func getDate() -> Date? {
+        return date
+    }
+    
+    public func getDateString() -> String? {
+        return dateString
+    }
+    
+    private func fromDateToString(
         date:Date,
         format:String = "yyyy-MM-dd'T'HH:mm:ss"
-    ) -> Self {
+    ) {
         let df = DateFormatter()
         df.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
         let locale = Locale(identifier: "en_US_POSIX")
         df.locale = locale
         let date = df.string(from: date)
         self.dateString = date
-        return self
     }
     
-    public func fromStringUTCToDate(
+    private func fromStringToDate(
         dateString:String
-    ) -> Self {
-        let format = detectFormatString(dateString)
+    ) {
         let df = DateFormatter()
-        df.dateFormat = format
+        df.dateFormat = detectFormatString(dateString)
         let locale = Locale(identifier: "en_US_POSIX")
         df.locale = locale
         df.timeZone = TimeZone(identifier:"UTC")
         let dateFromString = df.date(from: dateString)
         self.date = dateFromString
-        return self
     }
     
     public func detectFormatString(_ dateString:String) -> String {
-        let regex1 = "^d{4}-d{2}-d{2}Td{2}:d{2}:d{2}$"
-        let regex2 = "^d{4}-d{2}-d{2}Td{2}:d{2}:d{2}.d{3}$"
+        let regex1 = "^\\d{4}-\\d{2}-\\d{2}[']T[']\\d{2}:\\d{2}:\\d{2}$"
+        let regex2 = "^\\d{4}-\\d{2}-\\d{2}[']T[']\\d{2}:\\d{2}:\\d{2}.\\d{3}$"
         
-        if NSPredicate(format: "SELF MATCHES %@", regex2).evaluate(with: dateString.trimmingCharacters(in:.whitespacesAndNewlines)) {
-            return "yyyy-MM-ddTHH:mm:ss.SSS"
+        if NSPredicate(format: "SELF MATCHES %@", regex2).evaluate(with: dateString) {
+            return "yyyy-MM-dd'T'HH:mm:ss.SSS"
         }
-        return "yyyy-MM-ddTHH:mm:ss"
+        return "yyyy-MM-dd'T'HH:mm:ss"
     }
 }
