9c36848bd8a29884c7f3db09b7bd53bb7fcfbd3f
dai <phamdaiit@gmail.com>
2024-03-04 20:29:13 +0700

    updated

--- Sources/MyLibrary/Extensions/UIImage+Ext.swift
+++ Sources/MyLibrary/Extensions/UIImage+Ext.swift
@@ -59,6 +59,28 @@ public extension UIImage {
         return newImage
     }
     
+    func rotate(radians: Float) -> UIImage? {
+        var newSize = CGRect(origin: CGPoint.zero, size: self.size).applying(CGAffineTransform(rotationAngle: CGFloat(radians))).size
+        // Trim off the extremely small float value to prevent core graphics from rounding it up
+        newSize.width = floor(newSize.width)
+        newSize.height = floor(newSize.height)
+        
+        UIGraphicsBeginImageContextWithOptions(newSize, false, self.scale)
+        guard let context = UIGraphicsGetCurrentContext() else { return nil }
+        
+        // Move origin to middle
+        context.translateBy(x: newSize.width/2, y: newSize.height/2)
+        // Rotate around middle
+        context.rotate(by: CGFloat(radians))
+        // Draw the image at its center
+        self.draw(in: CGRect(x: -self.size.width/2, y: -self.size.height/2, width: self.size.width, height: self.size.height))
+    
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return newImage
+    }
+    
     func maskRoundedImage(radius: CGFloat? = nil) -> UIImage {
         let width = min(self.size.width,self.size.height)
         let imageView: UIImageView = UIImageView(frame: CGRect(origin: CGPoint.zero, size: CGSize(width: width, height: width)))
