aa1de4529b070eec23968a2088862ccae255207f
dai pham <dai.pham@vietunion.com.vn>
2024-07-20 11:38:54 +0700

    s

--- Sources/MyLibrary/Components/DropDownTree/DropdownSectionView.swift
+++ Sources/MyLibrary/Components/DropDownTree/DropdownSectionView.swift
@@ -12,6 +12,7 @@ class DropdownSectionView: UITableViewHeaderFooterView {
     @IBOutlet weak var contentLabel: UILabel!
     @IBOutlet weak var actionButton: UIButton!
     @IBOutlet weak var selectButton: ContainerButton!
+    @IBOutlet weak var action2Button: ContainerButton!
     
     var onTap:((Bool) -> Void)?
     var onSelect:((DropDownTreeItem) -> Void)?
@@ -20,6 +21,7 @@ class DropdownSectionView: UITableViewHeaderFooterView {
     override func awakeFromNib() {
         super.awakeFromNib()
         actionButton.addTarget(self, action: #selector(self.didTap(_:)), for: .touchUpInside)
+        action2Button.addTarget(self, action: #selector(self.didTap(_:)), for: .touchUpInside)
         selectButton.addTarget(self, action: #selector(self.didSelect(_:)), for: .touchUpInside)
         actionButton.setCheckBoxStyle(image: Resource.Icon.chevronRight, selectedImage: Resource.Icon.chevronDown)
     }
